6329062062d67efe67acfe531c73e6c21c6a6d75
Zamo98 <guzamora@estudiantec.cr>
2021-09-19 15:27:03 -0600

    cambio

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow ventana;
     ventana.show();
+    cout<<"Hola Mundo"<<endl;
     return a.exec();
 }
